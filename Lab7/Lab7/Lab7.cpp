c4d4858346d41695c8f99823a50c796c493df286
grig20051501 <root@User-PC.localdomain>
2023-03-11 18:42:29 +0300

    linear solution

--- Lab7/Lab7/Lab7.cpp
+++ Lab7/Lab7/Lab7.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-string text[100] = { "word well" };
+string text[100] = { "word woll" };
 
 string purify(string str) {
     string word = str;
@@ -35,9 +35,34 @@ vector<string> split(string str[]) {
     return words;
 }
 
+bool isEqual(string word1, string word2, int count) {
+    string str1 = word1.substr(0, count), str2 = word2.substr(0, count);
+    return str1 == str2;
+}
+
 int main()
 {
+    int numOfOne = 0, numOfTwo = 0, numOfThree = 0, numOfFour = 0;
+    
+    vector<string> words = split(text);
+    string word1 = words[0];
 
+    for (int i = 1; i < words.size(); i++) {
+        if (isEqual(word1, words[i], 1)) {
+            numOfOne++;
+        }
+        if (isEqual(word1, words[i], 2)) {
+            numOfTwo++;
+        }
+        if (isEqual(word1, words[i], 3)) {
+            numOfThree++;
+        }
+        if (isEqual(word1, words[i], 4)) {
+            numOfFour++;
+        }
+    }
 
+    cout << "one: " << numOfOne << " two: " << numOfTwo << " three: " << numOfThree << " four: " << numOfFour;
+ 
     return 0;
 }
